d34ce70e14a09f41b7715329cb62d6274763c77d
sr1g19 <sr1g19@soton.ac.uk>
2022-04-20 16:09:41 +0100

    Big commit

--- test.sh
+++ test.sh
@@ -4,47 +4,71 @@ echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Road 2"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_2_road2.g" 
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Road 3"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_3_road3.g" 
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Triangle"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_3_triangle.g"
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Road 4"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_4_road4.g"
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Road Y"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_4_roadY.g"
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Square"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_4_square.g"
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Road 5"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_5_road5.g"
 echo -
 echo -
 echo -
 echo -
-echo "Next Data"
+echo "Plus"
+echo -
+echo -
+echo -
 ./GSI.exe "data/data/$1" "data/query/_5_plus.g"

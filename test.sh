2c3bd9d6cfc3d56e66753d143f80bc968ed2d067
sr1g19 <sr1g19@soton.ac.uk>
2022-04-20 15:44:29 +0100

    Benchmark commit

--- test.sh
+++ test.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 echo "data/data/$1"
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_2_road2.g" 
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_3_road3.g" 
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_3_triangle.g"
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_4_road4.g"
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_4_roadY.g"
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_4_square.g"
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_5_road5.g"
-echo:
-echo:
-echo:
+echo -
+echo -
+echo -
+echo -
 echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_5_plus.g"

80eee960b781084bf2294bed45f531e58ea1b8df
sr1g19 <sr1g19@soton.ac.uk>
2022-04-20 15:38:59 +0100

    Big commit

--- test.sh
+++ test.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 echo "data/data/$1"
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_2_road2.g" 
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_3_road3.g" 
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_3_triangle.g"
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_4_road4.g"
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_4_roadY.g"
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_4_square.g"
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_5_road5.g"
-echo \n\n\n\n "Next Data"
+echo.
+echo.
+echo.
+echo "Next Data"
 ./GSI.exe "data/data/$1" "data/query/_5_plus.g"

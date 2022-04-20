6adadc50c379b344fbf45755bc364eda3dad1ffa
sr1g19 <sr1g19@soton.ac.uk>
2022-04-20 15:09:25 +0100

    Benchmark commit

---
+++ benchmark.sh
@@ -0,0 +1,8 @@
+./GSI.exe "data\%1" "query\_2_road2.g" 
+./GSI.exe "data\%1" "query\_3_road3.g" 
+./GSI.exe "data\%1" "query\_3_triangle.g"
+./GSI.exe "data\%1" "query\_4_road4.g"
+./GSI.exe "data\%1" "query\_4_roadY.g"
+./GSI.exe "data\%1" "query\_4_square.g"
+./GSI.exe "data\%1" "query\_5_road5.g"
+./GSI.exe "data\%1" "query\_5_plus.g"
